1bd617d852d9e635eaf56fefaa9a65fdb1556809
Thomas Kehl <tk@symas.ch>
2022-08-22 10:26:36 +0200

    Add project files.

---
+++ HelloWorldApp/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace HelloWorldApp {
+    public static class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
+// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
